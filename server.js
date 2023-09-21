0b4e2b83e3193d3f50f28c6db2ed471c5adaeb1b
Sarah Goodell <sarahlgoodell1@gmail.com>
2023-09-21 17:09:06 -0400

    Added in environment variables using .env

---
+++ server.js
@@ -0,0 +1,26 @@
+//Require Express Package
+const express = require('express');
+
+//Require Sequelize for connection.js
+const sequelize = require('sequelize');
+require(`dotenv`).config()
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Connect to the database before starting the Express.js server
+const runner = async () => {
+    try {
+     await sequelize.sync();
+     app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
+    } catch (err) {
+     console.error(err)
+    }
+   
+   }
+   
+   runner();
+   
