cd1c6b9dff4afc2195de35f94740b8955dcca966
Sarah Goodell <sarahlgoodell1@gmail.com>
2023-09-25 12:55:14 -0400

    Changed dashes to underscores in schema.sql

--- db/schema.sql
+++ db/schema.sql
@@ -11,7 +11,7 @@ DROP TABLE IF EXISTS departments;
 -- id
 -- name: varchar(30)
 CREATE TABLE departments (
-    id INT AUTO-INCREMENT PRIMARY KEY,
+    id INT NOT NULL AUTO_INCREMENT,
     name VARCHAR(30) NOT NULL,
     PRIMARY KEY(id)
 );
@@ -22,7 +22,7 @@ CREATE TABLE departments (
 -- salary: decimal
 -- department_id: int (foreign key)
 CREATE TABLE roles (
-    id INT AUTO-INCREMENT PRIMARY KEY,
+    id INT NOT NULL AUTO_INCREMENT,
     title VARCHAR(30) NOT NULL,
     salary INT NOT NULL,
     department_id INT,
@@ -38,7 +38,7 @@ CREATE TABLE roles (
 -- role_id: int (foreign key)
 -- manager_id: int (reference to another employee)
 CREATE TABLE employees (
-    id INT AUTO-INCREMENT PRIMARY KEY,
+    id INT NOT NULL AUTO_INCREMENT,
     PRIMARY KEY(id),
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
